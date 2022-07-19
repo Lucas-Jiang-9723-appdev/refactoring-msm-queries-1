8edef725c49a10d4862ebca87888273bc5a626e5
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-19 23:40:43 +0000

    1st commit - definitions of instances

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,9 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    d_id = self.id
+    movies = Movie.where({:director_id => d_id}).order({:year => :asc})
+    return movies
+  end
 end
